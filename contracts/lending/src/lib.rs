55a29b9efc53fba12fdfd2481a57c3842069d37c
KeNgKawthar <seniorrbackenddev@gmail.com>
2026-07-28 15:42:06 +0100

    Standardise propchain_traits (#848)
    * Standardise propchain_traits
    
    Standardise propchain_traits
    
    * fixes
    
    fixes
    
    * fixes
    
    vfixes
    
    * fixes made
    
    fixes made
    
    * made another fix
    
    made another fix
    
    * trying to fix ci
    
    trying to fix ci

--- contracts/lending/src/lib.rs
+++ contracts/lending/src/lib.rs
@@ -7,6 +7,7 @@
 )]
 
 use ink::storage::Mapping;
+use propchain_traits::map_reentrancy;
 
 #[ink::contract]
 mod propchain_lending {
@@ -40,11 +41,7 @@ mod propchain_lending {
         RequestExpired,
     }
 
-    impl From<propchain_traits::ReentrancyError> for LendingError {
-        fn from(_: propchain_traits::ReentrancyError) -> Self {
-            LendingError::ReentrantCall
-        }
-    }
+    map_reentrancy!(LendingError => ReentrantCall);
 
     #[derive(
         Debug, Clone, PartialEq, scale::Encode, scale::Decode, ink::storage::traits::StorageLayout,
--- contracts/traits/src/lib.rs
+++ contracts/traits/src/lib.rs
@@ -9,6 +9,7 @@ pub mod crypto;
 pub mod di;
 pub mod errors;
 pub mod randomness;
+pub mod reentrancy;
 pub mod reentrancy_guard;
 
 pub mod types;
